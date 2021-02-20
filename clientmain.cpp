54ce3a2fa6ac2f9047e6e6f744834118f079060b
Joel Berg <joel.berg@hotmail.se>
2021-02-20 19:37:45 +0100

    Added SendTo and RecvFrom

--- clientmain.cpp
+++ clientmain.cpp
@@ -19,7 +19,7 @@
 
 int main(int argc, char *argv[]){
  
-  int errorStatus;
+  int errorIndex;
   int socketFD;
   char* serverIP;
   char* serverPort;
@@ -53,14 +53,14 @@ int main(int argc, char *argv[]){
   prep.ai_family = AF_UNSPEC;
   prep.ai_socktype = SOCK_DGRAM;
 
-  errorStatus = getaddrinfo(serverIP, serverPort, &prep, &server);
-  if(errorStatus != 0)
+  errorIndex = getaddrinfo(serverIP, serverPort, &prep, &server);
+  if(errorIndex != 0)
   {
-    fprintf(stderr, "Program stopped at getaddrinfo: %s\n", gai_strerror(errorStatus));
+    fprintf(stderr, "Program stopped at getaddrinfo: %s\n", gai_strerror(errorIndex));
     return 1;
   }
 
-  for(pointer = server; pointer != NULL; pointer->ai_next)
+  for(pointer = server; pointer != NULL; pointer = pointer->ai_next)
   {
     socketFD = socket(pointer->ai_family, pointer->ai_socktype, pointer->ai_protocol);
     if(socketFD == -1)
@@ -69,32 +69,95 @@ int main(int argc, char *argv[]){
     }
     break;
   }
-/*
-  errorStatus = connect(socketFD, pointer->ai_addr, pointer->ai_addrlen);
-  if(errorStatus == -1)
+
+  int socketFDtemp = socket(pointer->ai_family, pointer->ai_socktype, pointer->ai_protocol);
+  errorIndex = connect(socketFDtemp, pointer->ai_addr, pointer->ai_addrlen);
+  if(errorIndex == -1)
   {
     fprintf(stderr, "Unable to connect to server.\n");
     return 2;
   }
-*/
+
+  /*
+   IPV4 Adresser
+  */
+
+  char hostIP[INET_ADDRSTRLEN];
+  struct sockaddr_in hostAdress;
+  socklen_t len = sizeof hostAdress;
+  
+  memset(&hostAdress, 0, sizeof(hostAdress));
+  getsockname(socketFD, (struct sockaddr*)&hostAdress, &len);
+  inet_ntop(AF_INET, &hostAdress, hostIP, sizeof(hostIP));
+  printf("Host Address: %s:%d\n", hostIP, hostAdress.sin_port);
+
   char localIP[INET_ADDRSTRLEN];
   struct sockaddr_in localAdress;
-  socklen_t len = sizeof localAdress;
+  len = sizeof localAdress;
   
   memset(&localAdress, 0, sizeof(localAdress));
-  getsockname(socketFD, (struct sockaddr*)&localAdress, &len);
+  getsockname(socketFDtemp, (struct sockaddr*)&localAdress, &len);
   inet_ntop(AF_INET, &localAdress, localIP, sizeof(localIP));
   int localPort = ntohs(localAdress.sin_port);
+  close(socketFDtemp);
 
   printf("Local IP address: %s:%d\n", localIP, localPort);
-/*
-  Hantera IPV6 Adresser
-*/
-
   
+  //----------------------------------------------------------
+
+  /*
+    Hantera IPV6 Adresser
+  */
+
+  //----------------------------------------------------------
 
 
+  // SendTo()
+  int32_t sentBytes;
+  int32_t recvBytes;
+  struct calcMessage mBuffer;
+  struct calcProtocol pBuffer;
 
+  mBuffer.type = htons(22);
+  mBuffer.message = htonl(0);
+  mBuffer.protocol = htons(17);
+  mBuffer.major_version = htons(1);
+  mBuffer.minor_version = htons(0);
+
+  sentBytes = sendto(socketFD, &mBuffer, sizeof(mBuffer), 0, pointer->ai_addr, pointer->ai_addrlen);
+  if(sentBytes == -1)
+  {
+    fprintf(stderr, "Unable to send.\n");
+    exit(1);
+  }
+  printf("Sent %d bytes.\n", sentBytes);
+
+
+  // Recvfrom()
+  recvBytes = recvfrom(socketFD, &pBuffer, sizeof(pBuffer), 0, pointer->ai_addr, &pointer->ai_addrlen);
+  if(recvBytes == -1)
+  {
+    fprintf(stderr, "Unable to recieve\n");
+    exit (1);
+  }
+  printf("Recieved %d bytes\n", recvBytes);
+
+  uint16_t pType = ntohs(pBuffer.type);
+  uint16_t pMajor = ntohs(pBuffer.major_version);
+  uint16_t pMinor = ntohs(pBuffer.minor_version);
+  uint16_t pID = ntohl(pBuffer.id);
+  uint16_t pArith = ntohl(pBuffer.arith);
+  uint16_t firstIntValue = ntohl(pBuffer.inValue1);
+  uint16_t secondIntValue = ntohl(pBuffer.inValue2);
+  uint16_t intResult = ntohl(pBuffer.inResult);
+  double firstFloatValue = pBuffer.flValue1;
+  double secondFloatValue = pBuffer.flValue2;
+  double floatResult = pBuffer.flResult;
+
+  printf("Type: %d, Major: %d, Minor: %d, ID: %d, Arith: %d\n", pType, pMajor, pMinor, pID, pArith);
+  printf("First Int: %d, Second Int: %d\n",  firstIntValue, secondIntValue);
+  printf("First Float: %8.8g, Second Float: %8.8g\n", firstFloatValue, secondFloatValue);
+  freeaddrinfo(server);  
   close(socketFD);
 
   return 0;
